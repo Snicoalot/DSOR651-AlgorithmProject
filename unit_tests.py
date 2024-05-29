5e05fad4456358a393d49508d6b5a5d7bd010c2f
Snicoalot <snicoalot@gmail.com>
2024-05-29 12:47:15 +0000

    Done with unit testing

--- unit_tests.py
+++ unit_tests.py
@@ -38,6 +38,16 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(root.children[8].board, [0, 0, 0, 0, 0, 0, 0, 0, 1])
         self.assertEqual(root.children[0].player, -1)
 
+        # Test that wins, visits, and the childrens' children are all empty
+        self.assertEqual(root.children[6].wins, 0)
+        self.assertEqual(root.children[6].visits, 0)
+        self.assertEqual(root.children[3].wins, 0)
+        self.assertEqual(root.children[2].visits, 0)
+        self.assertEqual(root.children[8].wins, 0)
+        self.assertEqual(root.children[8].visits, 0)
+        self.assertEqual(root.children[2].children, [])
+        self.assertEqual(root.children[7].children, [])
+
         board = [0, 0, 1, -1, 0, 0, 0, 0, 1]
         player = -1
         root = Node(board, player)
@@ -83,18 +93,44 @@ class TestFuncs(unittest.TestCase):
         for child in root.children:
             child.wins = 10 - i
             i = i + 1
+            # Force the third node to have the highest score
             if child == root.children[3]:
                 child.wins = 50
         # Test that the best node is the third node 
         node = root.select()
         self.assertEqual(node, root.children[3])
 
+    # This is a stochastic function, so it is difficult to test for all cases.
+    # We will make basic unit tests as a result
     def test_simulation(self):
-        board = [0, 0, 1, -1, 0, 0, 0, 0, 1]
+        # Set up player 1 as the winner, checking to return a winner
+        board = [1, -1, 0, -1, 1, 0, 0, 0, 1]
         player = -1
         root = Node(board, player)
-        
+        result = root.simulation()
+        self.assertEqual(result, 1)
 
+        # Set up player 2 as the winner, checking to return a winner
+        board = [-1, -1, -1, 1, -1, 1, 0, 0, 1]
+        player = 1
+        root = Node(board, player)
+        result = root.simulation()
+        self.assertEqual(result, -1)
+
+        # Set up a tie, checking to return a 0
+        board = [1, 1, -1, -1, -1, 1, 1, -1, 1]
+        player = 1
+        root = Node(board, player)
+        result = root.simulation()
+        self.assertEqual(result, 0)
+
+        # Set up for a tie after two simulations, checking to return a 0
+        board = [-1, 1, -1, 1, -1, 1, 0, -1, 1]
+        player = 1
+        root = Node(board, player)
+        result = root.simulation()
+        self.assertEqual(result, 0)
+        
     def test_checkWin(self):
         # Test for a winning row
         board = [1, 1, 1, 0, 0, 0, 0, 0, 0]
@@ -130,11 +166,40 @@ class TestFuncs(unittest.TestCase):
         gamemode = tictactoe.chooseGame(game)
         self.assertEqual(gamemode, 2)
 
-    # TODO: Find a way to test for an input
-    # def test_getMove(self):
-    #     board = [0, 0, 0, 0, 1, 0, 0, 0, 0]
-    #     move = getMove(board)
-    #     self.assertEqual(move, 4)
+    def test_getMove(self):
+        # Set up a board and request a player input
+        board = [1, 1, 1, 1, 0, 1, 1, 1, 1]
+        player = -1
+        print(f"To test the getMove function, press 4 on your keyboard.")
+        move = tictactoe.getMove(board)
+        board[move] = player
+
+        # Test that the input produces the correct result
+        self.assertEqual(move, 4)
+        self.assertEqual(board, [1, 1, 1, 1, -1, 1, 1, 1, 1])
+
+        # Set up a board and request a player input
+        board = [0, 1, 1, 1, 1, 1, 1, 1, 1]
+        player = -1
+        print(f"To test the getMove function, press 0 on your keyboard.")
+        move = tictactoe.getMove(board)
+        board[move] = player
+
+        # Test that the input produces the correct result
+        self.assertEqual(move, 0)
+        self.assertEqual(board, [-1, 1, 1, 1, 1, 1, 1, 1, 1])
+
+        # Set up a board and request a player input
+        board = [-1, -1, -1, -1, -1, -1, -1, -1, 0]
+        player = 1
+        print(f"To test the getMove function, press 8 on your keyboard.")
+        move = tictactoe.getMove(board)
+        board[move] = player
+
+        # Test that the input produces the correct result
+        self.assertEqual(move, 8)
+        self.assertEqual(board, [-1, -1, -1, -1, -1, -1, -1, -1, 1])
+
 
     def test_printBoard(self):
         # Capture the output of the printBoard function
@@ -154,6 +219,49 @@ class TestFuncs(unittest.TestCase):
         sys.stdout = captured_output
         tictactoe.printBoard(board)
         self.assertEqual(captured_output.getvalue(), expected_output)
+
+    # Because MCTS uses the simulation function, which is inherently stochastic, testing will be limited to
+    # very specific scenarios.
+    def test_mcts(self):
+        board = [0, 1, 1, 0, -1, 0, 0, 0, 0]
+        player = -1
+        sims = 2000
+        root = Node(board, player)
+        # Test for MCTS to block the opponent without winning the board
+        board = tictactoe.mcts(board, player, sims)
+        self.assertEqual(board, [-1, 1, 1, 0, -1, 0, 0, 0, 0])
+
+        board = [1, 0, 0, 0, -1, -1, 0, 1, 0]
+        player = 1
+        sims = 2000
+        root = Node(board, player)
+        # Test for MCTS to block the opponent without winning the board
+        board = tictactoe.mcts(board, player, sims)
+        self.assertEqual(board, [1, 0, 0, 1, -1, -1, 0, 1, 0])
+
+        board = [-1, 1, 1, 0, -1, 1, 0, 0, 0]
+        player = -1
+        sims = 2000
+        root = Node(board, player)
+        # Test for MCTS to select the winning position in the board
+        board = tictactoe.mcts(board, player, sims)
+        self.assertEqual(board, [-1, 1, 1, 0, -1, 1, 0, 0, -1])
+
+        board = [1, 1, 0, -1, -1, 0, 0, 0, 0]
+        player = 1
+        sims = 2000
+        root = Node(board, player)
+        # Test for MCTS to block the opponent
+        board = tictactoe.mcts(board, player, sims)
+        self.assertEqual(board, [1, 1, 0, -1, -1, 1, 0, 0, 0])
+
+        board = [1, 1, 0, -1, -1, 1, 0, 1, -1]
+        player = -1
+        sims = 2000
+        root = Node(board, player)
+        # Test for MCTS to force a tie
+        board = tictactoe.mcts(board, player, sims)
+        self.assertEqual(board, [1, 1, -1, -1, -1, 1, 0, 1, -1])
 
 # Load the tests
 test_loader = unittest.TestLoader().loadTestsFromTestCase(TestFuncs)
